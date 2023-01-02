565648a7a1dd0677578d4f2005f4fd4021d6812d
Asiya <asiya_ermolaeva@mail.ru>
2023-01-02 19:29:20 +0300

    add pictures, categories states, change classNames

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,26 @@
+import React, { useState } from 'react'
+
 function Categories(){
+	const [activeIndex, setActiveIndex] =useState(0);
+
+	const onClickCateory = (index) => {
+		setActiveIndex(index);
+	}
 	return(
-	  <div class="categories">
+	  <div className="categories">
 		<ul>
-		  <li class="active">Все</li>
-		  <li>Мясные</li>
-		  <li>Вегетарианская</li>
-		  <li>Гриль</li>
-		  <li>Острые</li>
-		  <li>Закрытые</li>
+			<li onClick={() => onClickCateory(0)} 
+			  className = {activeIndex === 0 ? "active":''}>Все</li>
+			<li onClick={() => onClickCateory(1)} 
+			  className = {activeIndex === 1 ? "active":''}>Мясные</li>
+			<li onClick={() => onClickCateory(2)} 
+			  className = {activeIndex === 2 ? "active":''}>Вегетарианские</li>
+			<li onClick={() => onClickCateory(3)} 
+			  className = {activeIndex === 3 ? "active":''}>Гриль</li>
+			<li onClick={() => onClickCateory(4)} 
+			  className = {activeIndex === 4 ? "active":''}>Острые</li>
+			<li onClick={() => onClickCateory(5)} 
+			  className = {activeIndex === 5 ? "active":''}>Закрытые</li>
 		</ul>
 	  </div>
 	)
