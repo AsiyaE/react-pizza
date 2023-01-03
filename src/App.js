b9f1f5c766c59794b8681768d0d4fe9a8b37ba6f
Asiya <asiya_ermolaeva@mail.ru>
2023-01-03 23:12:07 +0300

    Create pizza, category component, render the list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 
-import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizza.json';
 
-import './scss/app.scss'
+import './scss/app.scss';
 
 function App() {
 	return (
-		<div className="wrapper">
-			<Header/>
-			<div className="content">
-				<div className="container">
-					<div className="content__top">
-						<Categories />
-						<Sort />
-					</div>
-					<h2 className="content__title">Все пиццы</h2>
-					<div className="content__items">
-						<PizzaBlock title="Мексиканская" price={500}/>
-						<PizzaBlock title="Гавайская" price={350}/>
-					</div>
-				</div>
+	<div className="wrapper">
+		<Header />
+		<div className="content">
+		<div className="container">
+			<div className="content__top">
+			<Categories />
+			<Sort />
+			
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{pizzas.map((pizza) => (
+					<PizzaBlock {...pizza} />
+				))}
 			</div>
 		</div>
+		</div>
+	</div>
 	);
 }
 
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,29 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function Categories(){
-	const [activeIndex, setActiveIndex] =useState(0);
+function Categories() {
+	const [activeIndex, setActiveIndex] = useState(0);
 
+	const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые'];
 	const onClickCateory = (index) => {
 		setActiveIndex(index);
-	}
-	return(
-	  <div className="categories">
-		<ul>
-			<li onClick={() => onClickCateory(0)} 
-			  className = {activeIndex === 0 ? "active":''}>Все</li>
-			<li onClick={() => onClickCateory(1)} 
-			  className = {activeIndex === 1 ? "active":''}>Мясные</li>
-			<li onClick={() => onClickCateory(2)} 
-			  className = {activeIndex === 2 ? "active":''}>Вегетарианские</li>
-			<li onClick={() => onClickCateory(3)} 
-			  className = {activeIndex === 3 ? "active":''}>Гриль</li>
-			<li onClick={() => onClickCateory(4)} 
-			  className = {activeIndex === 4 ? "active":''}>Острые</li>
-			<li onClick={() => onClickCateory(5)} 
-			  className = {activeIndex === 5 ? "active":''}>Закрытые</li>
-		</ul>
-	  </div>
-	)
+	};
+	
+	return (
+		<div className="categories">
+			<ul>
+				{categories.map((value, i) => (
+					<li
+						onClick={() => onClickCateory(i)}
+						className={activeIndex === i ? 'active' : ''}>
+						{value}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default Categories;
