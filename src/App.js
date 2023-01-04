393698442781fd12a987b31c14bb1640b2f2193d
Asiya <asiya_ermolaeva@mail.ru>
2023-01-04 22:58:54 +0300

    get data from the backend, add the useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizza.json';
 
 import './scss/app.scss';
 
 function App() {
+	const [items, setItems] = useState([]);
+
+	useEffect(()=> {
+		fetch('https://63b5cb8258084a7af3a08c7f.mockapi.io/items')
+		.then((res) => res.json())
+		.then((arr) => {
+			console.log(arr)
+			setItems(arr);
+		})
+	}, [])
+
 	return (
 	<div className="wrapper">
 		<Header />
@@ -21,7 +31,7 @@ function App() {
 			</div>
 			<h2 className="content__title">Все пиццы</h2>
 			<div className="content__items">
-				{pizzas.map((pizza) => (
+				{items.map((pizza) => (
 					<PizzaBlock {...pizza} />
 				))}
 			</div>
