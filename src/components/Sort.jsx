35ee3f86bc67d12ddffea9179b16e6261528d607
Asiya <asiya_ermolaeva@mail.ru>
2023-01-18 21:48:58 +0300

    transfer filtering and sorting states to Redux

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSort } from '../redux/slices/filterSlice';
 
-function Sort({ value, onChangeSort }) {
+function Sort() {
+  const dispatch = useDispatch();
+  const sort = useSelector((state)=> state.filter.sort)
+
   const [open, setOpen] = useState(false);
+
+  const onClickListItem = (obj) => {
+    dispatch(setSort(obj));
+    setOpen(false);
+  };
 
   const list = [
     { name: 'популярности (DESC)', sortProperty: 'rating' },
@@ -11,11 +21,6 @@ function Sort({ value, onChangeSort }) {
     { name: 'алфавиту (DESC)', sortProperty: 'title' },
     { name: 'алфавиту (ASK)', sortProperty: '-title' },
   ];
-
-  const onClickListItem = (i) => {
-    onChangeSort(i);
-    setOpen(false);
-  };
 
   return (
     <div className="sort">
@@ -32,7 +37,7 @@ function Sort({ value, onChangeSort }) {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span onClick={() => setOpen(!open)}>{value.name}</span>
+        <span onClick={() => setOpen(!open)}>{sort.name}</span>
       </div>
       {open && (
         <div className="sort__popup">
@@ -41,7 +46,7 @@ function Sort({ value, onChangeSort }) {
               <li
                 key={i}
                 onClick={() => onClickListItem(obj)}
-                className={value.sortProperty === obj.sortProperty ? 'active' : ''}>
+                className={sort.sortProperty === obj.sortProperty ? 'active' : ''}>
                 {obj.name}
               </li>
             ))}
